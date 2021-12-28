c012b41b8bdf6b257ba73874c0d23fb4f1c5d52d
Ricardo <ricperezrodriguez@gmail.com>
2021-12-28 17:47:50 +0100

    http ver pais

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent implements OnInit {
+  termino: string = ''
+
+  constructor( private paisService: PaisService ) { }
+
+  ngOnInit(): void {
+  }
+
+
+  buscar() {
+    console.log(this.termino);
+    this.paisService.buscarPais( this.termino )
+    .subscribe( resp => {
+      console.log(resp);
+    })
+  }
+
+
+
+}
