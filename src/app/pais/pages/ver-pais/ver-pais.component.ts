27d4a4945ab2d53d7a9f8c2fc22f245ea1eb3d5b
Ricardo <ricperezrodriguez@gmail.com>
2022-01-01 19:03:34 +0100

    pagina ver pais acabada

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -9,6 +11,8 @@ import { PaisService } from '../../services/pais.service';
   ]
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country; //si pones ! significa que considere en el codigo que tiene un valor
+                  //porque typescript da error porque en un principio es null
 
   constructor( 
     private activatedRoute: ActivatedRoute,
@@ -18,17 +22,26 @@ export class VerPaisComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.params
-      .subscribe( ({id}) => {
-
-        this.paisService.getPaisPorCodigo(id)
-          .subscribe( pais => {
-            console.log(pais);
-          })
-
+      .pipe(
+        switchMap( ({id}) => this.paisService.getPaisPorCodigo(id)),
+        tap(console.log)
+      )
+      .subscribe( pais => {
+        this.pais = pais[0];
       })
+
+    // this.activatedRoute.params
+    //   .subscribe( ({id}) => {
+
+    //     this.paisService.getPaisPorCodigo(id)
+    //       .subscribe( pais => {
+    //         console.log(pais);
+    //       })
+
+    //   })
   }
 
 
-  
+
 
 }
